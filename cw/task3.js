1daafd77aeee46e3d0bbcedb87d7860bad285164
Oleksandr-Heleta <sanock.geleta@gmail.com>
2020-12-03 11:05:35 +0200

    cw done

---
+++ cw/task3.js
@@ -0,0 +1,13 @@
+/* Task 3:
+            Напишите условие if для проверки, что переменная age находится в диапазоне между 14 и 90 включительно.
+            «Включительно» означает, что значение переменной age может быть равно 14 или 90. 
+            Значение вводит пользователь */
+
+var age = +prompt("Please, enter your age", 0);
+if (age >= 14 && age <= 90) {
+    console.log(age + " is true");
+    alert("You can see this film");
+} else {
+    console.log(age + " is false");
+    alert("You cann\'t see this film");
+}
