4c8a0b6b70c4dd745efddd13760bece204296ca1
Oleksandr-Heleta <sanock.geleta@gmail.com>
2020-12-23 23:47:49 +0200

    one more step to end

--- final_task/js/modal.js
+++ final_task/js/modal.js
@@ -1,6 +1,8 @@
 var popup = document.querySelector('.consult-wrapper');
 var btnShowModal = document.querySelector('.header__consult-btn');
 var btnClose = document.getElementById('popup_close');
+
+// popup.style.display = 'none';
         
     btnShowModal.addEventListener('click', function(){
             popup.style.display = 'block';
