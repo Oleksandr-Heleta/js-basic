993b1e17c4d86c35107e60f439039c55d9011b55
Oleksandr-Heleta <sanock.geleta@gmail.com>
2020-12-24 22:28:10 +0200

    ready to check

---
+++ final_task/js/service.js
@@ -0,0 +1,26 @@
+var items = document.getElementById("service_id");
+var wrap = document.getElementById('wrapp');
+var item = document.getElementById("item");
+var closeBtn = document.getElementById("show_close");
+// console.log(items);
+var  selected;
+
+items.onclick = function(event) {
+    let target = event.target; // где был клик?
+    // console.log(target);
+  if (target.className != 'service__item') {
+    target=event.target.closest(".service__item");
+  };
+    
+    item.appendChild(target);
+    wrap.style.display = 'block'; 
+  };
+  
+closeBtn.addEventListener('click', function() {
+  var delItem = item.lastChild;
+  items.appendChild(delItem);
+  
+  wrap.style.display = 'none';
+  
+}); 
+// Пожалуйста подробную рецензию на этот скрипт и Ваш вариант решения!!!!! 
